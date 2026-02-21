3a6d137050ede44ff5c70681353ae330413f2c53
omarroq2 <oliver.marroquin31217@gmail.com>
2026-02-20 21:11:36 -0500

    added for loop to compute totalScore from scores, then console.log(totalScore)

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 "use strict";
 
-// 1) Arrays
+// 1 & 2) Arrays
 const tags = ["js", "week3"];
 const scores = [10, 25, 18, 30];
+
+// 3 for loop to compute totalScore from scores, then console.log(totalScore)
+
+let totalScore = 0;
+
+for (let i = 0; i < scores.length; i++) {
+  totalScore += scores[i];
+}
+
+console.log("totalScore:", totalScore);
